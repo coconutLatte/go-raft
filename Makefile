b69d489f74dc13aecead67a71c7d158eff33757b
coconut_latte <690324981@qq.com>
2022-03-25 22:37:24 +0800

    init zap log

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+export GOPROXY=http://goproxy.cn
+
 default:
 	go mod tidy
 	go build -o bin/raft cmd/raft.go
---
+++ pkg/log/log.go
@@ -0,0 +1,49 @@
+package log
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+)
+
+var log *zap.SugaredLogger
+
+func InitSimpleLog() {
+	logger, err := zap.NewDevelopment(zap.AddCallerSkip(1))
+	if err != nil {
+		panic(fmt.Sprintf("init zap logger failed, %v", err))
+	}
+
+	log = logger.Sugar()
+}
+
+func Info(args ...interface{}) {
+	log.Info(args)
+}
+
+func Infof(template string, args ...interface{}) {
+	log.Infof(template, args)
+}
+
+func Debug(args ...interface{}) {
+	log.Debug(args)
+}
+
+func Debugf(template string, args ...interface{}) {
+	log.Debugf(template, args)
+}
+
+func Warn(args ...interface{}) {
+	log.Warn(args)
+}
+
+func Warnf(template string, args ...interface{}) {
+	log.Warnf(template, args)
+}
+
+func Error(args ...interface{}) {
+	log.Error(args)
+}
+
+func Errorf(template string, args ...interface{}) {
+	log.Errorf(template, args)
+}
